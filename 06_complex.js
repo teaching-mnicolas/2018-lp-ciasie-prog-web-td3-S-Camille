bacf5892d7be5ab76b8bc3854aeba4f785afa7fa
Camille SCHWARZ <camille.schwarz9@etu.univ-lorraine.fr>
2018-10-16 15:46:13 +0200

    Ajout du TD3 partie 3

--- 06_complex.js
+++ 06_complex.js
@@ -1,41 +1,41 @@
-describe ("Chain of prototypes", function() {
-  let warrior, toto, Character, Warrior;
+describe("Chain of prototypes", function() {
+    let warrior, toto, Character, Warrior;
 
-  beforeEach(function() {
-    Character = function() {};
-    warrior = new Character();
+    beforeEach(function() {
+        Character = function() {};
+        warrior = new Character();
 
-    Warrior = function() {};
-    Warrior.prototype = warrior;
-    toto = new Warrior();
-  });
+        Warrior = function() {};
+        Warrior.prototype = warrior;
+        toto = new Warrior();
+    });
 
-  it ("constructor of toto is Character, not Warrior", function() {
-    expect(toto.constructor).not.toBe(__);
-    expect(toto.constructor).toBe(__);
-  });
+    it("constructor of toto is Character, not Warrior", function() {
+        expect(toto.constructor).not.toBe(Warrior);
+        expect(toto.constructor).toBe(Character);
+    });
 
-  it ("__proto__ of toto is Warrior.prototype", function() {
-    expect(toto.__proto__).toBe(__);
-    expect(toto.__proto__).toBe(__);    // there is another name for this object
-  });
+    it("__proto__ of toto is Warrior.prototype", function() {
+        expect(toto.__proto__).toBe(Warrior.prototype);
+        expect(toto.__proto__).toBe(warrior); // there is another name for this object
+    });
 
-  it ("Character is the constructor of toto.__proto__", function() {
-    expect(toto.__proto__.constructor).toBe(__);
-    expect(warrior.constructor).toBe(__);
-  });
+    it("Character is the constructor of toto.__proto__", function() {
+        expect(toto.__proto__.constructor).toBe(Character);
+        expect(warrior.constructor).toBe(Character);
+    });
 
-  it ("toto.__proto__.__proto__ is NOT warrior", function() {
-    expect(toto.__proto__.__proto__).not.toBe(__);
-     expect(warrior.__proto__).not.toBe(__);
-  });
+    it("toto.__proto__.__proto__ is NOT warrior", function() {
+        expect(toto.__proto__.__proto__).not.toBe(Warrior);
+        expect(warrior.__proto__).not.toBe(Warrior);
+    });
 
-  it ("Character is the constructor of toto.__proto__.__proto__", function() {
-    expect(toto.__proto__.__proto__.constructor).toBe(__);
-  });
+    it("Character is the constructor of toto.__proto__.__proto__", function() {
+        expect(toto.__proto__.__proto__.constructor).toBe(Character);
+    });
 
-  it ("Character is the constructor of warrior.__proto__", function() {
-    expect(warrior.__proto__.constructor).toBe(__);
-  });
+    it("Character is the constructor of warrior.__proto__", function() {
+        expect(warrior.__proto__.constructor).toBe(Character);
+    });
 
 });
--- 07_redefinition.js
+++ 07_redefinition.js
@@ -1,35 +1,35 @@
-describe ("Prototype redefinition", function() {
-  let warrior, warrior2
+describe("Prototype redefinition", function() {
+    let warrior, warrior2
 
-  beforeEach(function() {
-    const Character = function() {}
-    Character.prototype.hp = 100
-    Character.prototype.armor = 10
+    beforeEach(function() {
+        const Character = function() {}
+        Character.prototype.hp = 100
+        Character.prototype.armor = 10
 
-    warrior = new Character()
+        warrior = new Character()
 
-    Character.prototype = {
-      hp: 150,
-      shout: function () {
-        return "FUS RO DAH !"
-      }
-    }
+        Character.prototype = {
+            hp: 150,
+            shout: function() {
+                return "FUS RO DAH !"
+            }
+        }
 
-    warrior2 = new Character()
-  })
+        warrior2 = new Character()
+    })
 
-  it ("Redefining the prototype does NOT modify existing instances", function() {
-    expect(warrior.hp).toBeDefined()
-    expect(warrior.hp).toBeEquals(__)
-    expect(warrior.armor).toBe__()
-    expect(warrior.shout).toBe__()
-  })
+    it("Redefining the prototype does NOT modify existing instances", function() {
+        expect(warrior.hp).toBeDefined()
+        expect(warrior.hp).toEqual(100)
+        expect(warrior.armor).toBeDefined()
+        expect(warrior.shout).toBeUndefined()
+    })
 
-  it ("Redefining the prototype does modify future instances", function() {
-    expect(warrior2.hp).toBeDefined()
-    expect(warrior2.hp).toBeEquals(__)
-    expect(warrior2.armor).toBe__()
-    expect(warrior2.shout).toBe__()
-  })
+    it("Redefining the prototype does modify future instances", function() {
+        expect(warrior2.hp).toBeDefined()
+        expect(warrior2.hp).toEqual(150)
+        expect(warrior2.armor).toBeUndefined()
+        expect(warrior2.shout).toBeDefined()
+    })
 
 })
