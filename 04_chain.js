dbaa15678aeb7f3ef20437ab59b4d29b1c273ec9
Camille SCHWARZ <camille.schwarz9@etu.univ-lorraine.fr>
2018-10-16 15:13:00 +0200

    Ajout du TD3 partie 2

--- 04_chain.js
+++ 04_chain.js
@@ -1,63 +1,63 @@
-describe ("Chain of prototypes", function() {
-  let toto, Character, Warrior;
+describe("Chain of prototypes", function() {
+    let toto, Character, Warrior;
 
-  beforeEach(function() {
-    Character = function() {};
-    Character.prototype.hp = 100;
+    beforeEach(function() {
+        Character = function() {};
+        Character.prototype.hp = 100;
 
-    const warrior = new Character();
-    warrior.damage = 42;
+        const warrior = new Character();
+        warrior.damage = 42;
 
-    Warrior = function() {};
-    Warrior.prototype = warrior;
+        Warrior = function() {};
+        Warrior.prototype = warrior;
 
-    toto = new Warrior();
-    toto.name = "toto";
-  });
-
-  describe ("Properties access", function() {
-    it ("object has access to its own property", function() {
-      expect(toto.name).toBeDefined();
-      expect(toto.name).toEqual(__);
+        toto = new Warrior();
+        toto.name = "toto";
     });
 
-    it ("object has access to property of its prototype", function() {
-      expect(toto.damage).toBeDefined();
-      expect(toto.damage).toEqual(__);
+    describe("Properties access", function() {
+        it("object has access to its own property", function() {
+            expect(toto.name).toBeDefined();
+            expect(toto.name).toEqual("toto");
+        });
+
+        it("object has access to property of its prototype", function() {
+            expect(toto.damage).toBeDefined();
+            expect(toto.damage).toEqual(42);
+        });
+
+        it("object has access to property of its prototype's prototype", function() {
+            expect(toto.hp).toBeDefined();
+            expect(toto.hp).toEqual(100);
+        });
+
+        it("Prototype modification is propagated for prototype's prototype too", function() {
+            expect(toto.mp).toBeUndefined();
+            Character.prototype.mp = 50; // add a property to toto prototype's prototype
+            expect(toto.mp).toBeDefined();
+            expect(toto.mp).toEqual(50);
+        });
     });
 
-    it ("object has access to property of its prototype's prototype", function() {
-      expect(toto.hp).toBeDefined();
-      expect(toto.hp).toEqual(__);
+    describe("instanceof", function() {
+        it("object is instance of its constructor", function() {
+            expect(toto instanceof Warrior).toBeTruthy(); // its constructor
+        });
+
+        it("object is instance of its constructor's constructor", function() {
+            expect(toto instanceof Character).toBeTruthy(); // its constructor's constructor
+        });
     });
 
-    it ("Prototype modification is propagated for prototype's prototype too", function() {
-      expect(toto.mp).toBeUndefined();
-      Character.__.mp = 50;                 // add a property to toto prototype's prototype
-      expect(toto.mp).toBeDefined();
-      expect(toto.mp).toEqual(__);
-    });
-  });
+    describe("object", function() {
+        it("every object is instance of Object", function() {
+            expect(toto instanceof Object).toBeTruthy(); // check that toto is also an instance of object
+        });
 
-  describe ("instanceof", function() {
-    it ("object is instance of its constructor", function() {
-      expect(toto instanceof __).toBeTruthy();  // its constructor
+        it("every object has access to properties of object", function() {
+            expect(toto.toString).toBeDefined();
+            expect(toto.toString()).toEqual("[object Object]"); // call the method toString()
+        });
     });
-
-    it ("object is instance of its constructor's constructor", function() {
-      expect(toto instanceof __).toBeTruthy();  // its constructor's constructor
-    });
-  });
-
-  describe ("object", function() {
-    it ("every object is instance of Object", function() {
-      expect(toto instanceof __).toBeTruthy(); // check that toto is also an instance of object
-    });
-
-    it ("every object has access to properties of object", function() {
-      expect(toto.toString).toBeDefined();
-      expect(toto.__).toEqual("[object Object]"); // call the method toString()
-    });
-  });
 
 });
--- 05_strangeness.js
+++ 05_strangeness.js
@@ -1,149 +1,153 @@
-describe ("Strangeness", function() {
+describe("Strangeness", function() {
 
-  describe ("Mutables", function() {
-    it ("{} is of type Object", function() {
-      expect(({}) instanceof Object).toBe__();      // true or false
+    describe("Mutables", function() {
+        it("{} is of type Object", function() {
+            expect(({}) instanceof Object).toBeTruthy(); // true or false
+        });
+
+        it("[] is of type Array", function() {
+            expect([] instanceof Array).toBeTruthy(); // true or false
+        });
+
+        it("[] is of type Object also", function() {
+            expect([] instanceof Object).toBeTruthy(); // true or false
+        });
+
+        it("/aaa/ is of type RegExp", function() {
+            expect(/aaa/
+                instanceof RegExp).toBeTruthy(); // true or false
+        });
+
+        it("/aaa/ is of type Object also", function() {
+            expect(/aaa/
+                instanceof Object).toBeTruthy(); // true or false
+        });
+
+        it("f is of type Function", function() {
+            const f = function() {};
+            expect(f instanceof Function).toBeTruthy(); // true or false
+        });
+
+        it("f is of type Object also", function() {
+            const f = function() {};
+            expect(f instanceof Object).toBeTruthy(); // true or false
+        });
     });
 
-    it ("[] is of type Array", function() {
-      expect([] instanceof Array).toBe__();       // true or false
+
+    describe("Immutables", function() {
+
+        describe("Number", function() {
+            it("1 is of type Number", function() {
+                expect(typeof 1).toEqual("number");
+            });
+
+            it("1 is NOT instance of Number", function() {
+                expect(1 instanceof Number).toBeFalsy(); // true or false
+            });
+
+            it("1 is NOT instance of Object", function() {
+                expect(1 instanceof Object).toBeFalsy(); // true or false
+            });
+
+            it("but new Number is a Number", function() {
+                const n = new Number();
+                expect(n instanceof Number).toBeTruthy(); // true or false
+            });
+        });
+
+        describe("String", function() {
+            it("'' is of type String", function() {
+                expect(typeof '').toEqual("string");
+            });
+
+            it("'' is NOT instance of String", function() {
+                expect(''
+                    instanceof String).toBeFalsy(); // true or false
+            });
+
+            it("'' is NOT instance of Object", function() {
+                expect(''
+                    instanceof Object).toBeFalsy(); // true or false
+            });
+
+            it("but new String is a String", function() {
+                const s = new String();
+                expect(s instanceof String).toBeTruthy(); // true or false
+            });
+        });
+
+        describe("Boolean", function() {
+            it("true is of type Boolean", function() {
+                expect(typeof true).toEqual("boolean");
+            });
+
+            it("true is NOT instance of Boolean", function() {
+                expect(true instanceof Boolean).toBeFalsy(); // true or false
+            });
+
+            it("true is NOT instance of Object", function() {
+                expect(true instanceof Object).toBeFalsy(); // true or false
+            });
+
+            it("but new Boolean is a Boolean", function() {
+                const b = new Boolean();
+                expect(b instanceof Boolean).toBeTruthy(); // true or false
+            });
+        });
+
     });
 
-    it ("[] is of type Object also", function() {
-      expect([] instanceof Object).toBe__();      // true or false
+    describe("Specials", function() {
+
+        describe("Infinity", function() {
+            it("Infinity is of type Number", function() {
+                expect(typeof Infinity).toEqual("number");
+            });
+
+            it("Infinity is NOT instance of Number", function() {
+                expect(Infinity instanceof Number).toBeFalsy(); // true or false
+            });
+
+            it("Infinity is NOT instance of Object", function() {
+                expect(Infinity instanceof Object).toBeFalsy(); // true or false
+            });
+        });
+
+        describe("NaN", function() {
+            it("NaN is of type Number", function() {
+                expect(typeof NaN).toEqual("number");
+            });
+
+            it("NaN is NOT instance of Number", function() {
+                expect(NaN instanceof Number).toBeFalsy(); // true or false
+            });
+
+            it("NaN is NOT instance of Object", function() {
+                expect(NaN instanceof Object).toBeFalsy(); // true or false
+            });
+        });
+
+        describe("undefined", function() {
+            it("undefined is of type undefined", function() {
+                expect(typeof undefined).toEqual("undefined");
+            });
+
+            it("undefined is NOT instance of Object", function() {
+                expect(undefined instanceof Object).toBeFalsy(); // true or false
+            });
+        });
+
+        describe("null", function() {
+            it("null is of type Object", function() {
+                expect(typeof null).toEqual("object");
+            });
+
+            it("null is NOT instance of Object", function() {
+                expect(null instanceof Object).toBeFalsy(); // true or false
+            });
+        });
+
     });
-
-    it ("/aaa/ is of type RegExp", function() {
-      expect(/aaa/ instanceof RegExp).toBe__();   // true or false
-    });
-
-    it ("/aaa/ is of type Object also", function() {
-      expect(/aaa/ instanceof Object).toBe__();   // true or false
-    });
-
-    it ("f is of type Function", function() {
-      const f = function() {};
-      expect(f instanceof Function).toBe__();     // true or false
-    });
-
-    it ("f is of type Object also", function() {
-      const f = function() {};
-      expect(f instanceof Object).toBe__();       // true or false
-    });
-  });
-
-
-  describe ("Immutables", function() {
-
-    describe ("Number", function() {
-      it ("1 is of type Number", function() {
-        expect(typeof 1).toEqual("number");
-      });
-
-      it ("1 is NOT instance of Number", function() {
-        expect(1 instanceof Number).toBe__();     // true or false
-      });
-
-      it ("1 is NOT instance of Object", function() {
-        expect(1 instanceof Object).toBe__();     // true or false
-      });
-
-      it ("but new Number is a Number", function() {
-        const n = new Number();
-        expect(n instanceof Number).toBe__();     // true or false
-      });
-    });
-
-    describe ("String", function() {
-      it ("'' is of type String", function() {
-        expect(typeof '').toEqual("string");
-      });
-
-      it ("'' is NOT instance of String", function() {
-        expect('' instanceof String).toBe__();    // true or false
-      });
-
-      it ("'' is NOT instance of Object", function() {
-        expect('' instanceof Object).toBe__();    // true or false
-      });
-
-      it ("but new String is a String", function() {
-        const s = new String();
-        expect(s instanceof String).toBe__();     // true or false
-      });
-    });
-
-    describe ("Boolean", function() {
-      it ("true is of type Boolean", function() {
-        expect(typeof true).toEqual("boolean");
-      });
-
-      it ("true is NOT instance of Boolean", function() {
-        expect(true instanceof Boolean).toBe__();   // true or false
-      });
-
-      it ("true is NOT instance of Object", function() {
-        expect(true instanceof Object).toBe__();    // true or false
-      });
-
-      it ("but new Boolean is a Boolean", function() {
-        const b = new Boolean();
-        expect(b instanceof Boolean).toBe__();      // true or false
-      });
-    });
-
-  });
-
-  describe ("Specials", function() {
-
-    describe ("Infinity", function() {
-      it ("Infinity is of type Number", function() {
-        expect(typeof Infinity).toEqual("number");
-      });
-
-      it ("Infinity is NOT instance of Number", function() {
-        expect(Infinity instanceof Number).toBe__();      // true or false
-      });
-
-      it ("Infinity is NOT instance of Object", function() {
-        expect(Infinity instanceof Object).toBe__();      // true or false
-      });
-    });
-
-    describe ("NaN", function() {
-      it ("NaN is of type Number", function() {
-        expect(typeof NaN).toEqual("number");
-      });
-
-      it ("NaN is NOT instance of Number", function() {
-        expect(NaN instanceof Number).toBe__();         // true or false
-      });
-
-      it ("NaN is NOT instance of Object", function() {
-        expect(NaN instanceof Object).toBe__();         // true or false
-      });
-    });
-
-    describe ("undefined", function() {
-      it ("undefined is of type undefined", function() {
-        expect(typeof undefined).toEqual("undefined");
-      });
-
-      it ("undefined is NOT instance of Object", function() {
-        expect(undefined instanceof Object).toBe__();   // true or false
-      });
-    });
-
-    describe ("null", function() {
-      it ("null is of type Object", function() {
-        expect(typeof null).toEqual("object");
-      });
-
-      it ("null is NOT instance of Object", function() {
-        expect(null instanceof Object).toBe__();        // true or false
-      });
-    });
-
-  });
 
 });
